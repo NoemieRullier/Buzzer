f495dc7a01ba49b20d12eba14b6159e228c52164
Nomyx <r.noemie25@orange.fr>
2013-09-02 16:51:30 +0200

    Correction of the maquette and clear some elements on Arduino code

--- arduino/animation_buzzer/animation_buzzer.ino
+++ arduino/animation_buzzer/animation_buzzer.ino
@@ -5,20 +5,26 @@
  */
 
 // Connexion des boutons
-const int buzzer_1 = 2;
-const int buzzer_2 = 19;
-const int buzzer_3 = 20;
-const int button_reinit = 3;
+const int buzzer_1 = 19;
+const int buzzer_2 = 2;
+const int buzzer_3 = 3;
+const int button_reinit = 20;
+
+// Interruption des boutons
+const int interrupt_buzzer_1 = 4;
+const int interrupt_buzzer_2 = 0;
+const int interrupt_buzzer_3 = 1;
+const int interrupt_button_reinit = 3;
 
 // Connexion des Relais
-const int relais_1 = 4;
-const int relais_2 = 5;
-const int relais_3 = 6;
+const int relais_1 = 5;
+const int relais_2 = 6;
+const int relais_3 = 7;
 
 // Déclaration de l'état des LED/Relais et initialisation (elles sont éteintes)
-volatile int state_LED1 = HIGH; 
-volatile int state_LED2 = HIGH;
-volatile int state_LED3 = HIGH;
+volatile int state_LED_1 = HIGH; 
+volatile int state_LED_2 = HIGH;
+volatile int state_LED_3 = HIGH;
 
 // Déclaration du booléen indiquant qu'une lumière est déjà allumée
 volatile boolean already_light = false;
@@ -35,68 +41,69 @@ void setup(){
   pinMode(button_reinit,INPUT_PULLUP);
 
   // Interception des boutons
-  attachInterrupt(0, ActionAppui_0, RISING); // Button 1
-  attachInterrupt(1, ActionAppui_1, RISING); // Button Réinitialisation
-  attachInterrupt(3, ActionAppui_3, RISING); // Button 3
-  attachInterrupt(4, ActionAppui_4, RISING); // Button 2
-  
+  attachInterrupt(interrupt_button_reinit, ActionAppui_0, RISING); // Button Réinitialisation
+  attachInterrupt(interrupt_buzzer_1, ActionAppui_1, RISING); // Button 1
+  attachInterrupt(interrupt_buzzer_2, ActionAppui_2, RISING); // Button 2
+  attachInterrupt(interrupt_buzzer_3, ActionAppui_3, RISING); // Button 3
+
   // Définition des Relais
   pinMode(relais_1,OUTPUT);
   pinMode(relais_2,OUTPUT);
   pinMode(relais_3,OUTPUT); 
   
   // Initialisation des LEDS
-  digitalWrite(relais_1, state_LED1);
-  digitalWrite(relais_2, state_LED2);
-  digitalWrite(relais_3, state_LED3);
+  digitalWrite(relais_1, state_LED_1);
+  digitalWrite(relais_2, state_LED_2);
+  digitalWrite(relais_3, state_LED_3);
   
 }
 
 void loop(){
   
-  digitalWrite(relais_1, state_LED1);
-  digitalWrite(relais_2, state_LED2);
-  digitalWrite(relais_3, state_LED3);
-  delay(1000);
+  digitalWrite(relais_1, state_LED_1);
+  digitalWrite(relais_2, state_LED_2);
+  digitalWrite(relais_3, state_LED_3);
+  
 }
 
+// Dans le cas d'une réinitialisation
 void ActionAppui_0(){
+  if (already_light){
+    already_light = false;
+    if (digitalRead(relais_1) == LOW){
+      state_LED_1 = HIGH;
+    }
+    else if (digitalRead(relais_2) == LOW){
+      state_LED_2 = HIGH;
+    }
+    else if(digitalRead(relais_3) == LOW){
+      state_LED_3 = HIGH;
+    }
+  }
+}
+
+// Dans le cas d'un des boutons
+void ActionAppui_1(){
   if (!already_light){
     already_light = true;
-    state_LED1 = LOW; // On allume la LED n°1
+    state_LED_1 = LOW; // On allume la LED n°1
    // playBuzzer();
   }
 }
 
-// Dans le cas d'une réinitialisation
-void ActionAppui_1(){
-  if (already_light){
-    already_light = false;
-    if (digitalRead(relais_1) == LOW){
-      state_LED1 = HIGH;
-    }
-    else if (digitalRead(relais_2) == LOW){
-      state_LED2 = HIGH;
-    }
-    else if(digitalRead(relais_3) == LOW){
-      state_LED3 = HIGH;
-    }
+void ActionAppui_2(){
+  if (!already_light){
+    already_light = true;
+    state_LED_2 = LOW; // On allume la LED n°2
+   // playBuzzer();
   }
 }
 
 void ActionAppui_3(){
   if (!already_light){
     already_light = true;
-    state_LED3 = LOW; // On allume la LED n°3
+    state_LED_3 = LOW; // On allume la LED n°3
     //playBuzzer();
-  }
-}
-
-void ActionAppui_4(){
-  if (!already_light){
-    already_light = true;
-    state_LED2 = LOW; // On allume la LED n°2
-   // playBuzzer();
   }
 }
 
